7b4990940386e0b757f0b06ee55987ea951511ff
Aisiri Prasad <aisiri.prasad@gmail.com>
2025-07-24 09:43:38 -0400

    Navigation: Added a navigation stack and linked our SecondView page with our home.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        NavigationStack{
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("This is the home page!")
+            NavigationLink(destination: SecondView ()) {
+                Text("Click me!")
+            }
+            }
         }
-        .padding()
     }
 }
 
