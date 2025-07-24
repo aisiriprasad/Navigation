deb0f5f65e635a5822c5b8430885cace31ecf6ed
Aisiri Prasad <aisiri.prasad@gmail.com>
2025-07-24 09:57:05 -0400

    Added a navigation title, called home.

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,9 +12,19 @@ struct ContentView: View {
         NavigationStack{
         VStack {
             Text("This is the home page!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.green)
+                .multilineTextAlignment(.center)
             NavigationLink(destination: SecondView ()) {
                 Text("Click me!")
+                    .font(.title3)
+                    .fontWeight(.medium)
+                    .multilineTextAlignment(.center)
             }
+            .navigationTitle("home")
+            .navigationBarTitleDisplayMode(.inline)
+            //.navigationBarHidden(.true) -> hides the nav bar 
             }
         }
     }
